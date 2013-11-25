f6242f89c3b9fb84e4a976871d7b2bfcc33bb743
Spiros <MyMac@mymacs-macbook-air.fritz.box>
2013-11-25 23:20:46 +0100

    added ListWebsites View added ListWebsitesCtrl controller added websites.json file that includes data of websites added http request to fetch data from websites.json and store it in a $scope variable show data fetched from websites.json in ListWebsites View

--- app/js/app.js
+++ app/js/app.js
@@ -12,5 +12,6 @@ angular.module('myApp', [
 config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
   $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
-  $routeProvider.otherwise({redirectTo: '/view1'});
+  $routeProvider.when('/listWebsites', {templateUrl: 'partials/listWebsites.html', controller: 'ListWebsitesCtrl'});
+  $routeProvider.otherwise({redirectTo: '/listWebsites'});
 }]);
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,9 +3,14 @@
 /* Controllers */
 
 angular.module('myApp.controllers', []).
-  controller('MyCtrl1', [function() {
+    controller('MyCtrl1', [function() {
 
-  }])
-  .controller('MyCtrl2', [function() {
+    }])
+    .controller('MyCtrl2', [function() {
 
-  }]);
+    }])
+    .controller('ListWebsitesCtrl', ['$scope', '$http',function($scope, $http) {
+        $http.get('websites/websites.json').success(function(data) {
+            $scope.websites = data;
+        });
+    }]);
