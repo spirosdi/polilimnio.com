833d601cef553aad43ec204f283c03065aa907ae
Spiros <MyMac@mymacs-macbook-air.fritz.box>
2013-11-26 18:39:10 +0100

    added back button functionality

--- app/js/directives.js
+++ app/js/directives.js
@@ -8,4 +8,18 @@ angular.module('myApp.directives', []).
     return function(scope, elm, attrs) {
       elm.text(version);
     };
-  }]);
+  }]).
+    directive('backButton', function(){
+        return {
+            restrict: 'A',
+
+            link: function(scope, element, attrs) {
+                element.bind('click', goBack);
+
+                function goBack() {
+                    history.back();
+                    scope.$apply();
+                }
+            }
+        }
+    });
