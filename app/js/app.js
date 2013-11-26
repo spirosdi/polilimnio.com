69fa7773608a66f42fb9be0a2a5a5116511bc728
Spiros <MyMac@mymacs-macbook-air.fritz.box>
2013-11-26 16:10:09 +0100

    added website.html View/Route/Controller added flickr local json object added listWebsites view

--- app/js/app.js
+++ app/js/app.js
@@ -1,17 +1,20 @@
 'use strict';
-
+// global variable attached to $window
+var searchText = 'polilimnio';
 
 // Declare app level module which depends on filters, and services
 angular.module('myApp', [
-  'ngRoute',
-  'myApp.filters',
-  'myApp.services',
-  'myApp.directives',
-  'myApp.controllers'
-]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
-  $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
-  $routeProvider.when('/listWebsites', {templateUrl: 'partials/listWebsites.html', controller: 'ListWebsitesCtrl'});
-  $routeProvider.otherwise({redirectTo: '/listWebsites'});
-}]);
+        'ngRoute',
+        'myApp.filters',
+        'myApp.services',
+        'myApp.directives',
+        'myApp.controllers'
+    ]).
+    config(['$routeProvider', function($routeProvider) {
+        $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
+        $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
+        $routeProvider.when('/listWebsites', {templateUrl: 'partials/listWebsites.html', controller: 'ListWebsitesCtrl'});
+        $routeProvider.when('/website/:websiteId', {templateUrl: 'partials/website.html', controller: 'WebsiteCtrl'});
+        $routeProvider.otherwise({redirectTo: '/listWebsites'});
+    }]);
+
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,7 +1,5 @@
 'use strict';
-
 /* Controllers */
-
 angular.module('myApp.controllers', []).
     controller('MyCtrl1', [function() {
 
@@ -13,4 +11,39 @@ angular.module('myApp.controllers', []).
         $http.get('websites/websites.json').success(function(data) {
             $scope.websites = data;
         });
+    }])
+    /*controller of Website view*/
+    .controller('WebsiteCtrl', ['$scope','$routeParams', '$http','$window',function($scope, $routeParams, $http, $window) {
+
+        // function to handle response when website is Flickr
+        function handleFlickrResponse(data, urlPhoto){
+            var photoUrls=[];
+            data.photos.photo.forEach(function(entry) {
+                console.log(entry);
+                // http get to get single photo data
+                $http.get(urlPhoto+entry.id).success(function(data) {
+                    photoUrls.push(data);
+                });
+            });
+            $scope.photoUrls = photoUrls;
+        }
+
+        // http get to get local Website JSON object
+        $http.get('websites/'+$routeParams.websiteId+'.json').success(function(localWebsiteData) {
+
+            var urlListPhotos = localWebsiteData[0].urlListPhotos
+            var urlPhoto = localWebsiteData[0].urlPhoto;
+
+            $http.get(urlListPhotos+$window.searchText).success(function(data) {
+                switch($routeParams.websiteId)
+                {
+                    case 'flickr':
+                        handleFlickrResponse(data, urlPhoto );
+                        break;
+                    default:
+                        console.log('website handler has not been added')
+                }
+            });
+        });
+
     }]);
